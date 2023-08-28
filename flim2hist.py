954f2dc944c6a64c966c473539133ce0fcc42bb2
DavideBasello <davidebasello@gmail.com>
2023-08-28 11:26:22 +0200

    230828 - background tau_m gray

--- flim2hist.py
+++ flim2hist.py
@@ -21,6 +21,7 @@ def read_photons(file_asc, path):
 def get_tau_m(file_asc, path):
     # returns mean lifetime as weighted arithmetic mean tm = (sum(a_i*tau_i)/sum(a_i))
     for f in file_asc:
+        print(f)
         if "_a1[%]" in f:
             a1 = np.loadtxt(path + "\\" + f)
             a2 = np.subtract(100, a1)
@@ -32,17 +33,21 @@ def get_tau_m(file_asc, path):
     tm_lim = [np.min(tm[np.nonzero(tm)]), np.max(tm[np.nonzero(tm)])]
     tm = np.ma.masked_where(tm < 10, tm)
     cmap = plt.colormaps.get_cmap("afmhot").copy()
-    cmap.set_bad(color='black')
+    cmap.set_bad(color='gray')
     return tm, tm_lim, cmap
         
 if __name__ == "__main__":
     path_in = sys.argv[1]
+    print(path_in)
     file_in = read_flim_data(path_in)
     [im_photons, im_lim] = read_photons(file_in, path_in)
     [im_tau, tau_lim, tau_map] = get_tau_m(file_in, path_in)
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(21, 6))
     ax1.imshow(im_photons, interpolation="none", cmap="gray",vmin=im_lim[0],vmax=im_lim[1])
     ax2.hist(im_photons.flatten(), bins=256)
-    im3 = ax3.imshow(im_tau, interpolation="None", cmap=tau_map, vmin=tau_lim[0],vmax=tau_lim[1])
+    # tau_m min and max are based on tau_lim, i.e. maximum and minimum lifetime in the image
+    # for fixed limits us: vmin = 500, vmax = 2800 in the line below
+    # im3 = ax3.imshow(im_tau, interpolation="None", cmap=tau_map, vmin=tau_lim[0],vmax=tau_lim[1])
+    im3 = ax3.imshow(im_tau, interpolation="None", cmap=tau_map, vmin=500,vmax=3000)
     plt.colorbar(im3, ax=ax3, fraction=0.046, pad=0.04, orientation='vertical')
     plt.show()
